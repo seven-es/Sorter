68ad51037f1fd06e4d5fb2b2bf851f75e9bd92ec
seven-es <s202152750@kfupm.edu.sa>
2024-02-12 17:24:05 +0300

    git merge master --allow-unrelated-histories

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,5 +1,11 @@
 public class Sorter {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+
     }
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+}
 }
